80240ec3f879b4e4347fde5d34c94c02b139f0cf
thanawatjack <75745883+thanawatjack@users.noreply.github.com>
2023-08-24 00:25:25 +0700

    update commit

--- config/config.go
+++ config/config.go
@@ -1,9 +1,174 @@
 package config
 
-import "os"
+import (
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
 
 func envPath() string {
 	if len(os.Args) == 1 {
-
+		return ".env"
+	} else {
+		return os.Args[1]
 	}
+}
+
+func LoadCoinfig(path string) IConfig {
+	envMap, err := godotenv.Read(path)
+	if err != nil {
+		log.Fatalf("Load dotenv fialed: %v", err)
+	}
+	return &config{
+		app: &app{
+			host: envMap["APP_HOST"],
+			port: func() int {
+				p, err := strconv.Atoi(envMap["APP_PORT"])
+				if err != nil {
+					log.Fatalf("Load port fialed: %v", err)
+				}
+				return p
+			}(),
+			name:    envMap["APP_NAME"],
+			version: envMap["APP_VERSION"],
+			readTimeout: func() time.Duration {
+				t, err := strconv.Atoi(envMap["APP_READ_TIMEOUT"])
+				if err != nil {
+					log.Fatalf("Load read fialed: %v", err)
+				}
+				return time.Duration(int64(t)) * int64(math.Pow10(9))
+			}(),
+			writeTimeout: func() time.Duration {
+				t, err := strconv.Atoi(envMap["APP_WRTIE_TIMEOUT"])
+				if err != nil {
+					log.Fatalf("Load write fialed: %v", err)
+				}
+				return time.Duration(int64(t)) * int64(math.Pow10(9))
+			}(),
+			bodyLimit: func() int {
+				b, err := strconv.Atoi(envMap["APP_BODY_LIMIT"])
+				if err != nil {
+					log.Fatalf("Load body fialed: %v", err)
+				}
+				return b
+			}(),
+			filelimit: func() int {
+				b, err := strconv.Atoi(envMap["APP_FILE_LIMIT"])
+				if err != nil {
+					log.Fatalf("Load file limit fialed: %v", err)
+				}
+				return b
+			}(),
+			gcpbucket: envMap["APP_GCP_BUCKET"],
+		},
+		db: &db{
+			host: envMap["DB_HOST"],
+			port: func() int {
+				p, err := strconv.Atoi(envMap["DB_PORT"])
+				if err != nil {
+					log.Fatalf("Load db port fialed: %v", err)
+				}
+				return p
+			}(),
+			protocol: envMap["DB_PROTOCOL"],
+			username: envMap["DB_USERNAME"],
+			password: envMap["DB_PASSWORD"],
+			database: envMap["DB_DATABASE"],
+			sslMode:  envMap["DB_SSL_MODE"],
+			maxConnection: func() int {
+				p, err := strconv.Atoi(envMap["DB_MAX_CONNECTIONS"])
+				if err != nil {
+					log.Fatalf("Load db max conection fialed: %v", err)
+				}
+				return p
+			}(),
+		},
+		jwt: &jwt{
+			adminKey:  envMap["JWT_SECRET_KEY"],
+			secertKey: envMap["JWT_ADMIN_KEY"],
+			apiKey:    envMap["JWT_API_KEY"],
+			accessExpiresAt: func() int {
+				t, err := strconv.Atoi(envMap["JWT_ACCESS_EXPIRES"])
+				if err != nil {
+					log.Fatalf("Load access expire fialed: %v", err)
+				}
+				return t
+			}(),
+			refreshExpiresAT: func() int {
+				t, err := strconv.Atoi(envMap["JWT_REFRESH_EXPIRES"])
+				if err != nil {
+					log.Fatalf("Load refresh expire fialed: %v", err)
+				}
+				return t
+			}(),
+		},
+	}
+}
+
+type IConfig interface {
+	App() IAppConfig
+	Db() IDbConfig
+	Jwt() IJwtConfig
+}
+
+type IAppConfig interface {
+}
+
+type config struct {
+	app *app
+	db  *db
+	jwt *jwt
+}
+
+type app struct {
+	host         string
+	port         uint
+	name         string
+	version      string
+	readTimeout  time.Duration
+	writeTimeout time.Duration
+	bodyLimit    int
+	filelimit    int
+	gcpbucket    string
+}
+
+func (c *config) App() IAppConfig {
+	return nil
+}
+
+type IDbConfig interface {
+}
+
+type db struct {
+	host          string
+	port          int
+	protocol      string
+	username      string
+	password      string
+	database      string
+	sslMode       string
+	maxConnection int
+}
+
+func (c *config) Db() IDbConfig {
+	return nil
+}
+
+type IJwtConfig interface {
+}
+
+type jwt struct {
+	adminKey         string
+	secertKey        string
+	apiKey           string
+	accessExpiresAt  int //sec
+	refreshExpiresAT int //sec
+}
+
+func (c *config) Jwt() IJwtConfig {
+	return nil
 }
